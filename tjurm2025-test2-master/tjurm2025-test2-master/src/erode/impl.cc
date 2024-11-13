649e627a51ad2fce4d617a41d28098095d64fd09
sjz-tjum <3884830295@qq.com>
2024-11-13 10:53:32 +0800

    Update impl.cc

--- tjurm2025-test2-master/tjurm2025-test2-master/src/erode/impl.cc
+++ tjurm2025-test2-master/tjurm2025-test2-master/src/erode/impl.cc
@@ -43,12 +43,7 @@ std::vector<cv::Mat> erode(const cv::Mat& src_erode, const cv::Mat& src_dilate) 
      *     2. dilate 之后的图像中，图中的小脚被消除了(类似答案中的样子)
      *     以上两个检查点需要自己检查，满足条件 则输入 p 通过, 否则输入 f 表示不通过
      */
-    cv::Mat dst_erode, dst_dilate;
-
-    // TODO: 在这里实现你的代码
-  
-
-    cv::Mat gray_erode, gray_dilate;  
+   cv::Mat gray_erode, gray_dilate;  
     
     cv::Mat binary_erode, binary_dilate;
 
