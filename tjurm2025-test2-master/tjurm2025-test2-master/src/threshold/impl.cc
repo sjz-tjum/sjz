5e72eee2598639152d4a3902609617abf344776b
sjz-tjum <3884830295@qq.com>
2024-11-13 18:00:05 +0800

    Update impl.cc

--- tjurm2025-test2-master/tjurm2025-test2-master/src/threshold/impl.cc
+++ tjurm2025-test2-master/tjurm2025-test2-master/src/threshold/impl.cc
@@ -33,5 +33,6 @@ std::vector<cv::Mat> threshold(const cv::Mat& src, int threshold_value)
     cv::Mat gray, dst;  
     cv::cvtColor(src, gray, cv::COLOR_BGR2GRAY);  
     cv::threshold(gray, dst, threshold_value, 255, cv::THRESH_BINARY);  
-    return {gray, dst};  
+    std::vector<cv::Mat> result = {gray, dst};
+    return result;  
 } 
